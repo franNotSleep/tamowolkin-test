3e54bd04eaa23efc8bfa02a6d881f8ff1dc5fe0a
Francisco Jose <frano04930@gmail.com>
2026-04-21 11:24:09 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="app">
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button type="button" onClick={() => setCount((c) => c + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
